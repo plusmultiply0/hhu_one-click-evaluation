094989fcc084931711f6ea26e0ec23b928fd66be
jczhou <1051160112@qq.com>
2020-06-18 14:17:44 +0800

    add description for Greasemonkey

--- auto.js
+++ auto.js
@@ -1,3 +1,17 @@
+// ==UserScript==
+// @name        河海大学(常州)教务系统一键评教脚本
+// @namespace   HHuc
+// @version     1.0
+// @description 在教师评教页面显示五个按钮，根据需要即可一键全选并填充评语（适用于常州校区，江宁和西康路校区的可以试试）
+// @author      plusmultiply0
+// @match       http://202.119.113.135/*
+// @match       http://202.119.113.136/*
+// @match       http://202.119.113.147/*
+// @match       http://202.119.113.148/*
+// @run-at      document-idle
+// @grant       none
+// @license     MIT License
+// ==/UserScript==
 const rowItem = [2, 5, 8, 11, 14, 17, 20];
 const colItem = [1, 2, 3, 4, 5];
 
